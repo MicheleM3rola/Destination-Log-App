44262dd7902476796d3b65058252a37323f0aa16
NewbieWebDev <42526092+NewbieWebDev@users.noreply.github.com>
2020-09-03 15:57:15 +0800

    'Destination-app'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,158 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Axios from "axios";
+import { useState, useEffect } from "react";
+import ReactMapGL, { Marker, Popup } from "react-map-gl";
+import "./index.css";
+import LogEntryForm from "./componets/LogEntryForm";
 
 function App() {
+  const [logEntries, setLogEntries] = useState([]);
+  const [showPopup, setShowPopup] = useState({});
+  const [addEntryLocation, setAddEntryLocation] = useState(null);
+  const [viewport, setViewport] = useState({
+    width: "100vw",
+    height: "100vh",
+    latitude: 37.7577,
+    longitude: -122.4376,
+    zoom: 3,
+  });
+
+  const listLogEntries = async () => {
+    const res = await Axios.get("http://localhost:2000/api/logs");
+    const requestedEntry = res.data;
+    return setLogEntries(requestedEntry);
+  };
+
+  useEffect(() => {
+    listLogEntries();
+  }, []);
+
+  const showAddMarkerPoupup = (event) => {
+    const [longitude, latitude] = event.lngLat;
+    setAddEntryLocation({
+      latitude: latitude,
+      longitude: longitude,
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ReactMapGL
+      {...viewport}
+      mapStyle="mapbox://styles/mike8827/ckebih4sl1aef1ap4ff9xtmxv"
+      mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+      onViewportChange={(nextViewport) => setViewport(nextViewport)}
+      onDblClick={showAddMarkerPoupup}
+    >
+      {logEntries.map((entry) => {
+        return (
+          <React.Fragment key={entry._id}>
+            <Marker
+              latitude={entry.latitude}
+              longitude={entry.longitude}
+              offsetTop={-24}
+              offsetLeft={-12}
+            >
+              <div
+                onClick={() => {
+                  setShowPopup({
+                    [entry._id]: true,
+                  });
+                }}
+              >
+                <svg
+                  className="marker"
+                  style={{
+                    width: "24px",
+                    height: "24px",
+                  }}
+                  viewBox="0 0 24 24"
+                  stroke-width="2"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path>
+                  <circle cx="12" cy="10" r="3"></circle>
+                </svg>
+              </div>
+            </Marker>
+            {showPopup[entry._id] ? (
+              <Popup
+                latitude={entry.latitude}
+                longitude={entry.longitude}
+                closeButton={true}
+                closeOnClick={false}
+                dynamicPosition={true}
+                onClose={() => {
+                  setShowPopup({});
+                }}
+                anchor="top"
+              >
+                <div className="popup">
+                  <img src={entry.image} alt="image-log-entry" />
+                  <h3>{entry.title}</h3>
+                  <p>{entry.comments}</p>
+                  <small>
+                    visited on :{" "}
+                    {new Date(entry.visitDate).toLocaleDateString()}
+                  </small>
+                </div>
+              </Popup>
+            ) : null}
+          </React.Fragment>
+        );
+      })}
+      {addEntryLocation ? (
+        <>
+          <Marker
+            latitude={addEntryLocation.latitude}
+            longitude={addEntryLocation.longitude}
+            offsetTop={-24}
+            offsetLeft={-12}
+          >
+            <div>
+              <svg
+                className="marker"
+                style={{
+                  width: "24px",
+                  height: "24px",
+                }}
+                viewBox="0 0 24 24"
+                stroke-width="2"
+                fill="none"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path>
+                <circle cx="12" cy="10" r="3"></circle>
+              </svg>
+            </div>
+          </Marker>
+          <Popup
+            className="popup"
+            latitude={addEntryLocation.latitude}
+            longitude={addEntryLocation.longitude}
+            closeButton={true}
+            closeOnClick={false}
+            dynamicPosition={true}
+            onClose={() => {
+              setAddEntryLocation(null);
+            }}
+            anchor="top"
+          >
+            <div className="popup">
+              <LogEntryForm
+                onClose={() => {
+                  setAddEntryLocation(null);
+                  listLogEntries();
+                }}
+                location={addEntryLocation}
+              />
+            </div>
+          </Popup>
+        </>
+      ) : null}
+    </ReactMapGL>
   );
 }
 
---
+++ src/componets/LogEntryForm.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import Axios from "axios";
+import { useForm } from "react-hook-form";
+
+import "../index.css";
+
+const LogEntryForm = ({ location, onClose }) => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = async (data) => {
+    try {
+      setLoading(true);
+      data.latitude = location.latitude;
+      data.longitude = location.longitude;
+      const entryLog = await Axios.post(
+        "http://localhost:2000/api/logs",
+        data,
+        {
+          headers: {
+            "content-type": "application/json",
+          },
+        }
+      );
+      onClose();
+    } catch (error) {
+      console.error(error);
+      setError(error.message);
+      setLoading(false);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} className="entry-form">
+      {error ? <h3 className="error">{error}</h3> : null}
+      <label htmlFor="title">Title</label>
+      <input type="text" name="title" required ref={register} />
+      <label htmlFor="comments">Comments</label>
+      <textarea name="comments" rows={3} ref={register}></textarea>
+      <label htmlFor="description">Description</label>
+      <textarea name="description" rows={3} ref={register}></textarea>
+      <label htmlFor="image">Image</label>
+      <input type="text" name="image" ref={register} />
+      <label htmlFor="visitDate">Visit Date</label>
+      <input type="date" name="visitDate" required ref={register} />
+      <button disabled={loading}>
+        {loading ? "Loading..." : "Create Entry"}
+      </button>
+    </form>
+  );
+};
+
+export default LogEntryForm;
